1f6d0715e961054642acde0e7cdb1da4389aec47
nik <122996793+nikhilchary129@users.noreply.github.com>
2024-09-09 19:38:44 +0530

    Time: 1159 ms (16.41%), Space: 290.2 MB (17.51%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,20 +5,13 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-2;i++){
             int j=i+1,z=nums.size()-1;
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+           
             while(j<z){
                 int store=nums[i]+nums[j]+nums[z];
                 if(store==0){
                     ans.insert({nums[i],nums[j],nums[z]});
                     j++,z--;
-                    while (j < z && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < z && nums[z] == nums[z + 1]) {
-                        z--;
-                    }
+                   
                 }else if(store<0){
                     j++;
                 }else  z--;
