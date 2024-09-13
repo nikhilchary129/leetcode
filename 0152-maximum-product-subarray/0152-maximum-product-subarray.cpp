933771b37883d8495a0d9c8cd8d18a4594c088c9
nik <122996793+nikhilchary129@users.noreply.github.com>
2024-09-13 09:11:33 +0530

    Time: 0 ms (100%), Space: 16.7 MB (46.48%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -10,10 +10,10 @@ public:
         int ans = nums[0];
         for (int i = 1; i < n; i++) {
             int temp = miner;           
-            miner = max({nums[i], miner * nums[i], maxer * nums[i]});   
-            maxer = min({nums[i], temp * nums[i], maxer * nums[i]});
+            miner = min({nums[i], miner * nums[i], maxer * nums[i]});   
+            maxer = max({nums[i], temp * nums[i], maxer * nums[i]});
             ans = max({ans, miner,maxer});
-            swap(miner,maxer);
+           // swap(miner,maxer);
         }
         return ans;
     }
